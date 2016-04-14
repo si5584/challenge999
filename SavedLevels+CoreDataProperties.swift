100ea05dfc1a61046c054c525a37d76c9f2fbd72
Simon Lovelock <simon@Simons-MacBook-Pro.local>
2016-04-14 18:16:41 +0100

    second commit - introducing saving stars for levels

--- SavedLevels+CoreDataProperties.swift
+++ SavedLevels+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  SavedLevels+CoreDataProperties.swift
 //  999challenge
 //
-//  Created by Simon Lovelock on 30/03/2016.
+//  Created by Simon Lovelock on 01/04/2016.
 //  Copyright © 2016 haloApps. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,15 +16,15 @@ extension SavedLevels {
 
     @NSManaged var challengeLevel: NSNumber?
     @NSManaged var challengeLives: NSNumber?
-    @NSManaged var infiniteLevel: NSNumber?
-    @NSManaged var infiniteLives: NSNumber?
+    @NSManaged var challengeRecord: NSNumber?
     @NSManaged var chillLevel: NSNumber?
     @NSManaged var chillLives: NSNumber?
+    @NSManaged var chillRecord: NSNumber?
+    @NSManaged var infiniteLevel: NSNumber?
+    @NSManaged var infiniteLives: NSNumber?
+    @NSManaged var infiniteRecord: NSNumber?
     @NSManaged var randomLevel: NSNumber?
     @NSManaged var randomLives: NSNumber?
-    @NSManaged var challengeRecord: NSNumber?
-    @NSManaged var chillRecord: NSNumber?
-    @NSManaged var infiniteRecord: NSNumber?
     @NSManaged var randomRecord: NSNumber?
 
 }
